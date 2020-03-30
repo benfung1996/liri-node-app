9cb4f6d6836f0c78ddc521092d31d262f843b9d8
Ben Fung <benfung1996@gmail.com>
2020-03-30 12:39:59 -0700

    axios bandsintown

--- liri.js
+++ liri.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
 
 var fs = require("fs");
+var axios = require("axios");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
-var bandsintown = require('bandsintown')(keys.bandsintown);
 var spotify = new Spotify(keys.spotify);
 
 
@@ -31,16 +31,15 @@ function UserInput (option, input) {
     };
 };
 
-
-function showConcert(input) {
-    bandsintown.getArtistEventList(input).then(function(error, events) {
-        if (!error) {
-            var concerts = JSON.parse(events);
-            for (var i = 0; i < concerts.length; i++) {
-                console.log("Name of the venue : " + concerts[i].venue.name);
-                console.log("Venue location : " + concerts[i].venue.city + concerts[i].venue.region + concerts[i].venue.country);
-                console.log("Date of the event : " + moment(concerts[i].venue.datatime)).format('L');
+    function showConcert(input) {
+        axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("Name of the venue : " + response.data[i].venue.name);
+                console.log("Venue location : " + response.data[i].venue.city + response.data[i].venue.region + response.data[i].venue.country);
+                console.log("Date of the event : " + moment(response.data[i].venue.datatime)).format('L');
             }
-        }
-    });
-}
+        }).catch(function (error) {
+            console.log(error);
+          })
+    }
