aab4c8dee9c7f99a6708db2bcc2f7bd6eca8a8ab
Ben Fung <benfung1996@gmail.com>
2020-03-31 16:38:57 -0700

    added movie-this function

--- liri.js
+++ liri.js
@@ -92,3 +92,49 @@ function UserInput (option, input) {
      
         });
     };
+
+    function showMoive(input) {
+        axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
+        .then(function(response) {
+            if (response.data.Title === undefined) {
+                showMoive("Mr. Nobody");
+            } else {
+                console.log("----------------------------------------------------------");
+                console.log("Title : " + response.data.Title);
+                console.log("Released : " + response.data.Released);
+                console.log("IMDB Rating : " + response.data.imdbRating);
+                    if (response.data.Ratings[1] === undefined) {
+                        console.log("No Rotten Tomatoes Score Available.");
+                    } else {
+                        console.log("Rotten Tomatoes Score : " + response.data.Ratings[1].Value);
+                    }
+                console.log("Country: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+                console.log("----------------------------------------------------------");
+            }
+            
+        })
+        .catch(function(error) {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log("---------------Data---------------");
+              console.log(error.response.data);
+              console.log("---------------Status---------------");
+              console.log(error.response.status);
+              console.log("---------------Status---------------");
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an object that comes back with details pertaining to the error that occurred.
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log("Error", error.message);
+            }
+            console.log(error.config);
+          });
+        
+    }
